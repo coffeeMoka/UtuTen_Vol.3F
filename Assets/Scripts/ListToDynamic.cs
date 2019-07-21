6d30364961dd23c833a9c02d85ec37d170b67463
Takuma Nagata <takuma.fn@gmail.com>
2019-07-21 22:18:37 +0900

    Add plugins.
    Uff8Jsonを再導入。

---
+++ Assets/Scripts/ListToDynamic.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+using Utf8Json;
+
+[System.Serializable]
+public class DynamicTable : Serialize.TableBase<string, string, DynamicPair> {
+}
+
+[System.Serializable]
+public class DynamicPair: Serialize.KeyAndValue<string, string> {
+    public DynamicPair(string key, string value) : base(key, value){}
+}
+public class ListToDynamic : MonoBehaviour {
+    [SerializeField]
+    private DynamicTable dynamicTable;
+    private Button button;
+    void Start() {
+        var filepath = $"{Application.dataPath}/type02.json";
+        Debug.Log(filepath);
+        button = this.gameObject.GetComponent<Button>();
+        button.OnClickAsObservable().Subscribe(_ => {
+            var table = dynamicTable.GetTable();
+            //Serialize.Serialization<string, string> serialization = new Serialize.Serialization<string, string>(table);
+            var json02 = JsonSerializer.ToJsonString(table);
+            JsonWrite(filePath: filepath, json: json02);
+        });
+    }
+    void JsonWrite(string filePath, string json) {
+        using(FileStream fs = new FileStream(filePath, FileMode.Append, FileAccess.Write))
+                using(StreamWriter s = new StreamWriter(fs))
+                    s.WriteLine(json);
+    }
+}
--- Assets/Scripts/Templates/Serialization.cs
+++ Assets/Scripts/Templates/Serialization.cs
@@ -4,38 +4,79 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-[Serializable]
-public class Serialization<T> {
-    [SerializeField]
-    List<T> target;
-    public List<T> ToList() { return target; }
-    public Serialization(List<T> t) {
-        this.target = t;
+namespace Serialize {
+    [Serializable]
+    public class TableBase<TKey, TValue, Type> where Type : KeyAndValue<TKey, TValue> {
+        [SerializeField]
+        private List<Type> list;
+        private Dictionary<TKey, TValue> table;
+        public Dictionary<TKey, TValue> GetTable() {
+            if(table == null) {
+                table = ListToDictionary(list);
+            }
+            return table;
+        }
+        public List<Type> GetList() {
+            if(list == null) {
+                list = new List<Type>();
+            }
+            return list;
+        }
+        static Dictionary<TKey, TValue> ListToDictionary(List<Type> list) {
+            var dic = new Dictionary<TKey, TValue>();
+            foreach(KeyAndValue<TKey, TValue> pair in list) {
+                dic.Add(pair.Key, pair.Value);
+            }
+            return dic;
+        }
     }
-}
+    [Serializable]
+    public class KeyAndValue<TKey, TValue> {
+        public TKey Key;
+        public TValue Value;
+        public KeyAndValue(TKey key, TValue value) {
+            Key = key;
+            Value = value;
+        }
+        public KeyAndValue(KeyValuePair<TKey, TValue> pair) {
+            Key = pair.Key;
+            Value = pair.Value;
+        }
+    }
 
-[Serializable]
-public class Serialization<TKey, TValue> : ISerializationCallbackReceiver {
-    [SerializeField]
-    List<TKey> keys;
-    [SerializeField]
-    List<TValue> values;
-    Dictionary<TKey, TValue> target;
-    public Dictionary<TKey, TValue> ToDictionary() { return target; }
-    public Serialization(Dictionary<TKey, TValue> t) {
-        this.target = t;
+    [Serializable]
+    public class Serialization<T> {
+        [SerializeField]
+        List<T> target;
+        public List<T> ToList() { return target; }
+        public Serialization(List<T> t) {
+            this.target = t;
+        }
     }
-    public void OnBeforeSerialize() {
-        keys = new List<TKey>(target.Keys);
-        values = new List<TValue>(target.Values);
-    }
-    public void OnAfterDeserialize() {
-        var count = Math.Min(keys.Count, values.Count);
-        target = new Dictionary<TKey, TValue>(count);
-        for(var i = 0; i < count; i++) {
-            var key = keys[i];
-            var value = values[i];
-            target.Add(key, value);
+
+    [Serializable]
+    public class Serialization<TKey, TValue> : ISerializationCallbackReceiver {
+        [SerializeField]
+        List<TKey> keys;
+        [SerializeField]
+        List<TValue> values;
+        Dictionary<TKey, TValue> target;
+        public Dictionary<TKey, TValue> ToDictionary() { return target; }
+        public Serialization(Dictionary<TKey, TValue> t) {
+            this.target = t;
+        }
+        public void OnBeforeSerialize() {
+            keys = new List<TKey>(target.Keys);
+            values = new List<TValue>(target.Values);
+        }
+        public void OnAfterDeserialize() {
+            var count = Math.Min(keys.Count, values.Count);
+            target = new Dictionary<TKey, TValue>(count);
+            for(var i = 0; i < count; i++) {
+                var key = keys[i];
+                var value = values[i];
+                target.Add(key, value);
+            }
         }
     }
 }
